5d2a1fa09a61ee254c9eb9988ad4a320b273f555
Davicho HF <39805786+David1044@users.noreply.github.com>
2021-05-27 15:46:14 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler'
+
+import React, {Component} from 'react';
+import {View, Image} from 'react-native';
+import MapView from 'react-native-maps'
+import MyDrawer from './MyDrawer'
+
+
+export default function App(){
+  return(
+    <MyDrawer/>
+  )
+}
+
